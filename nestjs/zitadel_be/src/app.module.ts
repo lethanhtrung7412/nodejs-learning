136ae3cb4f79d53809e9d9e569c0cc9e891a5622
Trung Le <lethanhtrung7412@gmail.com>
2025-07-29 09:34:25 +0700

    re-init project

--- nestjs/zitadel_be/src/app.module.ts
+++ nestjs/zitadel_be/src/app.module.ts
@@ -1,56 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { User } from './user/entities/user.entity';
-import { ZitadelAuthModule } from '@auth/zitadel-auth';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env', // explicitly specify the path
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        return {
-          type: 'postgres',
-          host: configService.get<string>('DB_HOST') as string,
-          port: parseInt(configService.get<string>('DB_PORT') || '5432'),
-          username: configService.get<string>('DB_USER') as string,
-          password: configService.get<string>('DB_PASSWORD') as string,
-          database: configService.get<string>('DB_NAME') as string,
-          entities: [User],
-          synchronize: true,
-        };
-      },
-    }),
-    ZitadelAuthModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        return {
-          authority: configService.getOrThrow('ZITADEL_AUTHORITY'),
-          authorization: {
-            type: 'jwt-profile',
-            profile: {
-              type: 'application',
-              keyId: configService.getOrThrow('ZITADEL_KEY_ID'),
-              key: configService.getOrThrow('ZITADEL_KEY'),
-              appId: configService.getOrThrow('ZITADEL_APP_ID'),
-              clientId: configService.getOrThrow('ZITADEL_CLIENT_ID'),
-            },
-          },
-        };
-      },
-    }),
-    UserModule,
-  ],
+  imports: [],
   controllers: [AppController],
-  providers: [ConfigService, AppService],
+  providers: [AppService],
 })
 export class AppModule {}
--- nestjs/zitadel_be/src/common/constant/auth.constants.ts
+++
@@ -1,4 +0,0 @@
-export const AUTH_CONSTANTS = {
-  ZITADEL_STRATEGY: 'zitadel-jwt',
-  BEARER_TOKEN: 'Bearer',
-} as const;
